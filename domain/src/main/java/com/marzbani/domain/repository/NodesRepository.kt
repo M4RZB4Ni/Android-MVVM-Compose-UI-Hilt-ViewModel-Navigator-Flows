1a61296904d36ab01d2c8fca0d31046c620f8441
Marzbani <hamied.reza.m@gmail.com>
2024-03-05 13:33:48 +0330

    [DOC] The Domain layer comments written for more readability and Dokka's usage

--- domain/src/main/java/com/marzbani/domain/repository/NodesRepository.kt
+++ domain/src/main/java/com/marzbani/domain/repository/NodesRepository.kt
@@ -1,15 +1,28 @@
+// NodesRepository.kt
 package com.marzbani.domain.repository
 
 import com.marzbani.domain.entity.DetailsEntity
 import com.marzbani.domain.entity.TreeNodeEntity
 import io.reactivex.Single
 
-// Repository interface for managing node-related data.
+/**
+ * Repository interface for managing node-related data.
+ */
 interface NodesRepository {
 
-    // Retrieves a list of tree nodes based on the provided URL.
+    /**
+     * Retrieves a list of tree nodes based on the provided URL.
+     *
+     * @param url The URL to fetch tree nodes from.
+     * @return A Single emitting a list of [TreeNodeEntity] representing the tree nodes.
+     */
     fun getNodes(url: String): Single<List<TreeNodeEntity>>
 
-    // Retrieves additional data for a specific data code.
+    /**
+     * Retrieves additional data for a specific data code.
+     *
+     * @param dataCode The code associated with the data for which additional information is requested.
+     * @return A Single emitting a [DetailsEntity] containing additional details.
+     */
     fun getAdditionalData(dataCode: String): Single<DetailsEntity>
 }
